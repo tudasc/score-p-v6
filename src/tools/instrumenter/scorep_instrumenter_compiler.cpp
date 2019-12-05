ebe990ce5415941cc33ea5018a83ada639a01a74
Jan Lehr <jp.lehr@gmail.com>
2019-12-05 16:09:41 +0100

    Changed path of plugin

--- src/tools/instrumenter/scorep_instrumenter_compiler.cpp
+++ src/tools/instrumenter/scorep_instrumenter_compiler.cpp
@@ -40,7 +40,7 @@
 #include <UTILS_IO.h>
 
 #define HAVE_LLVM_PLUGIN_SUPPORT true
-#define LLVM_PLUGIN_PATH "../../../../llvm-instrumenter/build/lib/instrumentationlib.so"
+#define LLVM_PLUGIN_PATH PIRA_SUBMODULE_PATH
 
 #if HAVE( POPEN )
 static void
