edc2179000d83822fbabce18458ca98cf0e881c2
Peter Arzt <peter.arzt@tu-darmstadt.de>
2024-04-02 16:38:43 +0200

    fixup! Adjust plugin loading to new LLVM version

--- set_instrumenter_directory.sh
+++ set_instrumenter_directory.sh
@@ -2,5 +2,4 @@
 
 instrumenterPath=$1
 
-sed -i "s;PIRA_SUBMODULE_PATH;\"$instrumenterPath\";g" ./src/tools/instrumenter/scorep_instrumenter_compiler.cpp
-sed -i "s;PIRA_SUBMODULE_PATH;\"$instrumenterPath\";g" ./src/tools/config/scorep_config_adapter.cpp
+sed -i "s;PIRA_SUBMODULE_PATH;\"$instrumenterPath\";g" ./src/tools/config/scorep_config_adapter.hpp
